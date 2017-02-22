a53473597085f2831b14ed0189544591f938cef6
stdgregwar <gregoirehirt@gmail.com>
2017-02-22 22:38:33 +0100

    holiday wip looks plugins. Need refactor of layout deployement system.

---
+++ BaseLookPlugin/BlockNodeLook.cpp
@@ -0,0 +1,6 @@
+#include "BlockNodeLook.h"
+
+BlockNodeLook::BlockNodeLook()
+{
+
+}
---
+++ BaseLookPlugin/BlockNodeLook.h
@@ -0,0 +1,11 @@
+#ifndef BLOCKNODELOOK_H
+#define BLOCKNODELOOK_H
+
+
+class BlockNodeLook
+{
+public:
+    BlockNodeLook();
+};
+
+#endif // BLOCKNODELOOK_H
