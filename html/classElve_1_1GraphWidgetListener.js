aad94b43d50348e46b35f81436d376973d5aeb89
Travis CI <travis@travis-ci.org>
2017-06-28 13:04:16 +0000

    Deploy code docs to GitHub Pages Travis build: 66
    Commit: aa1ae76b2ec8b7d589e0cda670c89c6d2f9c0730

--- html/classElve_1_1GraphWidgetListener.js
+++ html/classElve_1_1GraphWidgetListener.js
@@ -1,6 +1,7 @@
 var classElve_1_1GraphWidgetListener =
 [
     [ "GraphWidgetListener", "classElve_1_1GraphWidgetListener.html#a4b01c408547db53f27d80bb5e54439a0", null ],
-    [ "graphChanged", "classElve_1_1GraphWidgetListener.html#a8a7bbd8f2ac99bcccee364a5d8178c54", null ],
-    [ "runCommand", "classElve_1_1GraphWidgetListener.html#a14177ba62d5ab66955c157e67e6759cb", null ]
+    [ "graphChanged", "classElve_1_1GraphWidgetListener.html#a995c8358c49579758567dc395ecdffa1", null ],
+    [ "runCommand", "classElve_1_1GraphWidgetListener.html#a14177ba62d5ab66955c157e67e6759cb", null ],
+    [ "selectionChanged", "classElve_1_1GraphWidgetListener.html#a96eb78a96b778410eef508141ecd2270", null ]
 ];
